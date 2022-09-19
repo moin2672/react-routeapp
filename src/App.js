2426a9e62273cbe4f998a57ed98f91c37c24a700
moin2672 <moin2672@gmail.com>
2022-09-19 09:04:41 +0530

    2. adding links and navigation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavouritesPage from "./pages/Favourites";
+import MainNavigation from "./component/layout/MainNavigation";
 
 function App() {
   return (
-    <Routes>
+    <div>
+    <MainNavigation/>
+      <Routes>
       <Route index element={<AllMeetupsPage />} />
       <Route path="/home" element={<AllMeetupsPage />} />
       <Route path="/new-meetup" element={<NewMeetupPage />} />
       <Route path="/favourites" element={<FavouritesPage />} />
       <Route path="*" element={<Navigate replace to="/home" />} />
     </Routes>
+    </div>
   );
 }
 
---
+++ src/component/layout/MainNavigation.js
@@ -0,0 +1,20 @@
+import {Link} from 'react-router-dom'
+
+function MainNavigation(){
+    return(
+        <header>
+            <div>
+                React Meetups
+            </div>
+            <nav>
+                <ul>
+                    <li><Link to="/home">All Meetups</Link></li>
+                    <li><Link to="/new-meetup">Add New Meetup</Link></li>
+                    <li><Link to="/favourites">My Favourites</Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default MainNavigation

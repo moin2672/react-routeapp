e78c886d24610d30cd3a9c230475bc54ebc173e4
moin2672 <moin2672@gmail.com>
2022-09-20 12:42:58 +0530

    16. More context usage

--- src/component/layout/MainNavigation.js
+++ src/component/layout/MainNavigation.js
@@ -1,21 +1,31 @@
-import {Link} from 'react-router-dom'
-import classes from './MainNavigation.module.css';
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import FavouritesContext from "../../store/favourites-context";
+import classes from "./MainNavigation.module.css";
 
-function MainNavigation(){
-    return(
-        <header className={classes.header}>
-            <div className={classes.logo}>
-                React Meetups
-            </div>
-            <nav>
-                <ul>
-                    <li><Link to="/home">All Meetups</Link></li>
-                    <li><Link to="/new-meetup">Add New Meetup</Link></li>
-                    <li><Link to="/favourites">My Favourites</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
+function MainNavigation() {
+  const favouriteCntxt = useContext(FavouritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favourites">
+              My Favourites <span className={classes.badge}>{favouriteCntxt.totalFavourites}</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default MainNavigation
+export default MainNavigation;
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,7 +1,22 @@
+import { useContext } from "react"
+import MeetupList from "../component/meetups/MeetupList"
+import FavouritesContext from "../store/favourites-context"
+
 function FavouritesPage(){
+
+    const FavouritesCntxt = useContext(FavouritesContext)
+
+    let content;
+if(FavouritesCntxt.totalFavourites===0){
+    content = <p>You got no favourites yet. Start adding some? </p>
+}else{
+    content=<MeetupList meetups={FavouritesCntxt.favourites} />
+}
+
     return(
         <div>
-            Favourites Page
+           <h1>My Favourites Meetups</h1>
+           {content}
         </div>
     )
 }
